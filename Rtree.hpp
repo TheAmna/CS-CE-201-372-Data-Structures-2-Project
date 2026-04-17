0738b64275c20ae975c39d90295f77708ab3c513
Adina Naveed <adina.naveed19@gmail.com>
2026-04-17 16:33:28 +0500

    Add R-tree implementation in Rtree.hpp
    Implement R-tree structure with Rectangle and RtreeNode classes.

---
+++ Rtree.hpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <vector>
+#include <string>
+#pragma once
+
+struct Rectangle {
+    float x1,y1,x2,y2;
+    float area() const{
+        return (x2-x1)*(y2-y1);
+    }
+    bool overlaps(const Rectangle& other) const{
+        return !(x1>other.x2|| x2<other.x1 || y1>other.y2 || y2<other.y1)
+    }
+};
+
+struct RtreeNode{
+    bool isLeaf;
+    Rectangle mbr;
+    std::vector<Rectangle> items;
+    std::vector <RtreeNode*>children;
+    RtreeNode(bool leaf): isLeaf(leaf){}
+};
+
+class Rtree{
+    private:
+    RtreeNode* root_node;
+    int maxChildren;
+    int minChildren;
+    RtreeNode* choodeLeaf(RtreeNode* node,cinst Rectangle& rect); //finding the best sibling to put a new rectangle
+    void adjustTree(RtreeNode* node ); //update MBRs after an insertion
+    void splitNode( RtreeNode* node); //splitting after it overflows
+    float getEnlargement(const Rectangle& container, const Rectangle& newItem);//helper to calculate how much AREA WILL GROW WHEN WE ADD A RECTANGLE.
+
+
+    public:
+    Rtree(int maxChildren);
+    void insert(const Rectangle& rect);
+    void search(RtreeNode* node , const Rectangle& queryBox, std::vector<Rectangle>& results); //comparison
+    RtreeNode* getRoot(){return root_node;}
+    ~Rtree();
+};
